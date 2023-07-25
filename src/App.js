d50a6c0bcb220f6d50011dc8612b32a8374971e2
Tony Leng <lengtk@gmail.com>
2023-07-25 20:43:57 +0900

    added Submit Movie feature with onSubmitMovie function on Submit Movie button click

--- src/App.js
+++ src/App.js
@@ -2,32 +2,65 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { Auth } from './components/auth';
 import { db } from './config/firebase';
-import { getDocs, collection } from 'firebase/firestore';
+import { getDocs, collection, addDoc } from 'firebase/firestore';
 
 function App() {
   const [movieList, setMovieList] = useState([]);
+
+  const [newMovieTitle, setNewMovieTitle] = useState("");
+  const [newReleaseDate, setNewReleaseDate] = useState(0);
+  const [newGenre, setNewGenre] = useState("");
+
   const moviesCollectionRef = collection(db, "movies");
 
-  useEffect(() => {
-    // async function inside useEffect needs to have a function defined
-    const getMovieList = async () => {
-      try {
-        const data = await getDocs(moviesCollectionRef);
-        const filteredData = data.docs.map((doc)=>({...doc.data(), id:doc.id,}));
-        setMovieList(filteredData);
-      } catch (error) {
-        console.error(error);
-      }
+  const getMovieList = async () => {
+    try {
+      const data = await getDocs(moviesCollectionRef);
+      const filteredData = data.docs.map((doc)=>({...doc.data(), id:doc.id,}));
+      setMovieList(filteredData);
+    } catch (error) {
+      console.error(error);
     }
-
+  }
+  useEffect(() => {
     getMovieList();
-  }, [])
-    ;
+  }, []);
+
+  const onSubmitMovie = async () => {
+    try {
+      await addDoc(moviesCollectionRef, {
+        title: newMovieTitle,
+        releaseDate: newReleaseDate,
+        genre: newGenre
+      });
+
+      getMovieList();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className="App">
       <h1>Movie List</h1>
       <Auth />
+
+      <div>
+        <input
+          placeholder='Movie title...'
+          onChange={(e) => setNewMovieTitle(e.target.value)}
+        />
+        <input
+          placeholder='Release Date...'
+          type='number'
+          onChange={(e) => setNewReleaseDate(Number(e.target.value))}
+        />
+        <input
+          placeholder='genre...'
+          onChange={(e) => setNewGenre(e.target.value)}
+        />
+        <button onClick={onSubmitMovie}>Submit Movie</button>
+      </div>
 
       <div>
         {movieList.map((movie) => (
