3b1c3ac37c63dd2d54c4b943752c69e7a4cfe447
Tony Leng <lengtk@gmail.com>
2023-07-25 19:18:24 +0900

    initial commit, created React app and added firebase.js file and installed Firebase via CLI

---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjNhFg-x8yE_78ScwEIsA9njVJOGbgsG4",
+  authDomain: "movie-list-35de2.firebaseapp.com",
+  projectId: "movie-list-35de2",
+  storageBucket: "movie-list-35de2.appspot.com",
+  messagingSenderId: "140660222242",
+  appId: "1:140660222242:web:ed0590c0b04fd39b425d7b",
+  measurementId: "G-3VNGZDD84Q"
+};
+
+const app = initializeApp(firebaseConfig);
